6973769d6a1073f43d72dd518f9aad0a1a5371b4
HaroldDRF <95robots@gmail.com>
2019-10-14 18:49:30 -0700

    login form and template

--- clubAppFolder/users/views.py
+++ clubAppFolder/users/views.py
@@ -23,6 +23,25 @@ def register():
 
     return render_template('register.html', form=form)
 
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash('Log in Success!')
+
+            next = request.args.get('next')
+
+            if next == None or not next[0]=='/':
+                next = url_for('core.index')
+            
+            return redirect(next)
+
+    return render_template('login.html', form=form)
+
+
 @users.route('/logout')
 def logout():
     logout_user()
