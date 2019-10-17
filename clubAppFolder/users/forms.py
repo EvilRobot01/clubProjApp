9e5082d6beb68deff760480ef64d7b562b0ffb86
HaroldDRF <95robots@gmail.com>
2019-10-16 18:11:35 -0700

    register verif error fixed

--- clubAppFolder/users/forms.py
+++ clubAppFolder/users/forms.py
@@ -19,11 +19,11 @@ class RegistrationForm(FlaskForm):
     pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
-    def check_email(self, field):
+    def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('Your email has been registered already!')
 
-    def check_username(self, field):
+    def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Your username has been registered already!')
 
